0b83e69e6ca3a3daabf40b928e4ad1e624b47aa2
Max Pothier <pothiermaxwell@gmail.com>
2021-05-28 10:59:08 -0600

    Adding prices and names to DOM

--- web/src/components/PriceList.js
+++ web/src/components/PriceList.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import coingecko from '../api/coingecko';
+import '../styles/PriceList.css';
 
 class PriceList extends React.Component {
-  state = { prices: [] };
+  state = {
+    names: [],
+    currPrices: []
+  };
 
   componentDidMount() {
     this.loadAPI();
   }
 
   loadAPI = async () => {
-    const res = await coingecko.get('?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
+    const res = await coingecko.get('?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false');
+    this.populateState(res);
+  }
 
-    this.setState({ prices: res.data });
+  populateState(res) {
+    const coins = res.data.map((coin) => {
+      return <div>{coin.name}</div>
+    });
 
-    for (var i = 0, n = this.state.prices.length; i < n; i++) {
-      console.log(this.state.prices[i].name, this.state.prices[i].current_price);
-    }
+    const prices = res.data.map((price) => {
+      return <div>{price.current_price}</div>
+    });
+
+    this.setState({ currPrices: prices });
+    this.setState({ names: coins });
   }
 
   render() {
     return (
       <div>
         <h3>Price List</h3>
-        <span>{this.state.prices.length}</span>
+        <div className="row">
+          <div className="column">{this.state.names}</div>
+          <div className="column">{this.state.currPrices}</div>
+        </div>
       </div>
     );
   }
